1652594711ba513ff71b9c3e332e78004fd992f0
8327 <8327>
2022-03-13 20:26:08 +0000

    add ancestors.py

---
+++ ancestors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def read_proc_status(name):
+    file = open('/proc/' + str(name) + '/status')
+    lines = file.readlines()
+    parsed = {}
+    for line in lines:
+        tv = line.split(':')
+        tag = tv[0].strip()
+        value = tv[1].strip()
+        parsed[tag] = value
+    return parsed
+
+pid = os.getpid()
+if (len(sys.argv) > 1):
+    pid = sys.argv[1]
+while pid != '0':
+        tv = read_proc_status(pid)
+        print("%12s %s" % (tv['Pid'], tv['Name']))
+        pid = tv['PPid']
+
